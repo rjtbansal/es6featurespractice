cd9f81f236f313df4012f8053f446520d372e51e
Rajat Bansal <Rajat_Bansal@Trimble.com>
2016-10-07 13:33:24 -0700

    first commit

---
+++ features/default.js
@@ -0,0 +1,10 @@
+/*es6 feature*/
+
+/*below we are giving a default to name parameter so if no value is supplied 'world' is supplied as a default..this is es6 feature
+ */
+function sayHello(name='word'){
+    // name=name || 'world';   //earlier in es5 and below this is how we had to handle defaults
+    console.log('hello '+name+"!");
+}
+
+sayHello();
