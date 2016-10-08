b389df9dde2d944bc56d1f60b69789ca128e6008
rjtbansal <rjtbansal@yahoo.com>
2016-10-08 12:51:59 -0700

    used spread and rest params feature

--- features/spread-rest.js
+++ features/spread-rest.js
@@ -17,3 +17,23 @@ var merged_arr=[10, ...arr1, ...arr2, 25]; //we can also add remaining elements
 merged_arr.push(...[24,45,6]); //pushing more elements in array directly
 console.log(merged_arr);
 
+//using rest params(useful while passing args to functions)
+//first arg type will recieve the first parameter and the remaining will be added to nums
+function numbers(type,...nums){ //all args passed are getting coverted to an array
+    nums.forEach(function(num){
+        console.log(`Num=${num} is of the type ${type}`);
+    });
+}
+//passing multiple arguments
+numbers('numbers',3,34,5,12);
+
+
+//adder function..adding first number to rest of them
+function adder(adder, ...nums){
+    console.log(`Values recieved: ${adder},${nums}`);
+    nums.forEach(function(num){
+        console.log(`${adder}+${num}=${adder+num}`);
+    });
+}
+//2 should be added to remaining numbers
+adder(2,4,6,7);
